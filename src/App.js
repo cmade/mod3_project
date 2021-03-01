8bf84574cc91c9f1937b3172c643957239a6d7f4
cmade <cmangerere@gmail.com>
2021-03-01 15:58:25 -0500

    Edited the app.js file

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { ChakraProvider, theme } from '@chakra-ui/react';
 import { Navbar } from './components/Navbar';
 
 //The chakraProvider allows us to provide a theme to every component
 import { BrowserRouter as Router } from 'react-router-dom';
+import Loading from './components/Loading';
+import Footer from './components/Footer';
 
 export const App = () => {
-  return (
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 6000);
+  }, []);
+
+  return loading ? (
+    <ChakraProvider theme={theme}>
+      <Router>
+        <Loading />
+      </Router>
+    </ChakraProvider>
+  ) : (
     <ChakraProvider theme={theme}>
       <Router>
         <Navbar />
       </Router>
+      <Footer />
     </ChakraProvider>
   );
 };
