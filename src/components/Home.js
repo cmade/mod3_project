4bb7bc6b67fb4c65b5ba8b1dd24177b3df948a31
cmade <cmangerere@gmail.com>
2021-03-04 13:30:20 -0500

    Added Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,36 @@
 import { Box, Button, Heading, Text } from '@chakra-ui/react';
 import React from 'react';
 import { Link } from 'react-router-dom';
-export const Home = () => {
+import HelloWorldService, {
+  HelloWorldBeanService,
+} from '../api/HelloWorldService';
+
+export const Home = ({
+  user,
+  setUser,
+  welcome,
+  uWelcome,
+  bean,
+  uBean,
+  nami,
+  uNami,
+}) => {
+  const getMessage = async () => {
+    let res = await HelloWorldService();
+    let res2 = await HelloWorldBeanService();
+
+    console.log(res.data);
+    uWelcome(res.data);
+    uBean(res2.data.message);
+  };
+
   return (
-    <Box as="section" mt="-10">
+    <Box as="section" mt="2">
       <Box
         maxW="2xl"
         mx="auto"
         px={{ base: '6', lg: '8' }}
-        py={{ base: '16', sm: '20' }}
+        py={{ base: '16', sm: '10' }}
         textAlign="center"
       >
         <Heading as="h2" size="xl" fontWeight="extrabold" letterSpacing="tight">
@@ -23,8 +45,11 @@ export const Home = () => {
             as="a"
             href="#"
             size="sm"
-            colorScheme="blue"
+            bgGradient="linear(to-l, #6DF1D1,#5BDCFA)"
+            _hover={{ bgGradient: 'linear(to-l, green.300,blue.300)' }}
             fontWeight="bold"
+            color="white"
+            onClick={getMessage}
           >
             Start Free Trial
           </Button>
